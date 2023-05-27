9ddcbf5b1dc94927d4673683490ffa003eaad7b5
54chair <takukoshi3721@gmail.com>
2023-05-28 02:38:05 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+require('dotenv').config();
+
+// jsonを使えるようにする
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const { Configuration, OpenAIApi } = require("openai");
+
+app.post('/api', async (req, res) => {
+  const apiKey = req.body.apiKey;
+  const wordLang = req.body.wordLang;
+  const wordName = req.body.wordName;
+  const wordMean = req.body.wordMean;
+  const configuration = new Configuration({
+    apiKey: apiKey,
+  });
+  const openai = new OpenAIApi(configuration);  
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: `${wordLang}言語の${wordName}という${wordMean}という意味の単語を用いて簡単な例文を作成してください`}],
+  });
+  res.json({"contetnt": completion.data.choices[0].message.content});
+})
+
+app.listen(3000)
