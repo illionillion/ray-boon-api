7fec1bcb65629af91646d5bc768b29caec3a1116
54chair <takukoshi3721@gmail.com>
2023-06-03 21:41:06 +0900

    imgのルーティング

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('node:path');
 
 // jsonを使えるようにする
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use("/img", express.static(path.join(__dirname, "/public/img")));
 
 const { Configuration, OpenAIApi } = require("openai");
 
