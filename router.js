4955f1e692166f0f341ce8ed6349ab4bcbf20ee9
54chair <takukoshi3721@gmail.com>
2023-06-05 20:56:23 +0900

    generateExampleのモジュール化

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const { Configuration, OpenAIApi } = require("openai");
+const generateExample = require("./generateExample");
 
 router.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -14,19 +13,6 @@ router.get('/PrivacyPolicy', (req, res) => {
 router.get('/how-to-setting', (req, res) => {
   res.sendFile(__dirname + '/public/HowToSetting.html');
 })
-
-const generateExample = async (apiKey, wordLang, wordName, wordMean) => {
-  const configuration = new Configuration({
-    apiKey: apiKey,
-  });
-  const openai = new OpenAIApi(configuration);
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: `Create one simple short example sentence in the ${wordLang} language, always using the word ${wordName}, which means ${wordMean}. Include a Japanese translation at the end, and use only words of the same difficulty level as ${wordName}.` }],
-  });
-  const response = { "content": completion.data.choices[0].message.content };
-  return response;
-}
 
 router.post('/api', async (req, res) => {
   const apiKey = req.body.apiKey;
