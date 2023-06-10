c57572229cb280de9e20c6786841783aefa6531a
54chair <takukoshi3721@gmail.com>
2023-06-10 12:41:28 +0900

    不正なパスでアクセスされた時のエラーハンドリングの追加

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const ExpressError = require('./utils/ExpressErorr')
 const generateExample = require("./generateExample");
+const catchAsync = require("./utils/catchAsync")
 
 router.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -14,13 +16,21 @@ router.get('/how-to-setting', (req, res) => {
   res.sendFile(__dirname + '/public/HowToSetting.html');
 })
 
-router.post('/api', async (req, res) => {
-  const apiKey = req.body.apiKey;
-  const wordLang = req.body.wordLang;
-  const wordName = req.body.wordName;
-  const wordMean = req.body.wordMean;
+router.post('/api', catchAsync(async (req, res) => {
+  const { apiKey, wordLang, wordName, wordMean} = req.body;
   const result = await generateExample(apiKey, wordLang, wordName, wordMean);
   res.json(result);
+}))
+
+// 不正なパスがアクセスされた時の処理
+router.all('*', (req, res, next) => {
+  next(new ExpressError('ページが見つかりませんでした', 404));
+});
+
+// エラーハンドリング
+router.use((err, req, res, next) => {
+  const {statusCode = 500, message = '問題が起きました'} = err;
+  res.status(statusCode).json({"status": statusCode, "message": message})
 })
 
 module.exports = router;
