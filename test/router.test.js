f036318983a89addb69e08f55fc05fa75190420f
54chair <takukoshi3721@gmail.com>
2023-07-03 11:49:43 +0900

    Add API test for GET request

---
+++ test/router.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const express = require('express');
+const router = require('../router');
+
+const app = express();
+app.use(express.json());
+app.use('/', router);
+
+describe('API Tests', () => {
+  test('GET / should return index.html', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('text/html');
+  });
+
+  test('GET /privacy-policy should return PrivacyPolicy.html', async () => {
+    const response = await request(app).get('/privacy-policy');
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('text/html');
+  });
+
+  test('GET /how-to-setting should return HowToSetting.html', async () => {
+    const response = await request(app).get('/how-to-setting');
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('text/html');
+  });
+});
