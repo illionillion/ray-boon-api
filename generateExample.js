a7168167609534a739d0eca20d2283a7e916d104
54chair <takukoshi3721@gmail.com>
2023-07-14 12:10:25 +0900

    feat: 例文と例文の訳を分けてjsonで取得できるようにする

--- generateExample.js
+++ generateExample.js
@@ -6,10 +6,32 @@ const generateExample = async (apiKey, wordLang, wordName, wordMean) => {
   });
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: `Create one simple short example sentence in the ${wordLang} language, always using the word ${wordName}, which means ${wordMean}. Include a Japanese translation at the end, and use only words of the same difficulty level as ${wordName}.` }],
+    model: "gpt-3.5-turbo-0613",
+    messages: [{ role: "user", content: `${wordLang}で${wordMean}という意味の${wordName}という単語があります。この単語を使った例文と、例文を日本語訳した文を出力してください` }],
+    functions: [
+      {
+        "name": "generate_example",
+        "description": "generate example",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "example_sentence": {
+              "type": "string",
+              "description": "generated exapmle sentense."
+            },
+            "example_sentence_translated": {
+              "type": "string",
+              "description": "Generated example sentence translated into Japanese"
+            }
+          }
+        },
+        required: ["example_sentence", "example_sentence_translated"]
+      }
+    ],
+    function_call: "auto"
   });
-  const response = { "content": completion.data.choices[0].message.content };
+
+  const response = JSON.parse(completion.data.choices[0].message.function_call.arguments);
   return response;
 };
 
