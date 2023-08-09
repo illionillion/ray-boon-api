f2d4837fb4bec96084329714a3363f775a138b68
takeki <takekidanmkk@gmail.com>
2023-08-10 00:07:15 +0900

    generateExampleV1.jsを作成

--- generateExample.js
+++ generateExample.js
@@ -1,13 +1,13 @@
 const { Configuration, OpenAIApi } = require("openai");
 
-const generateExample = async (apiKey, wordLang, wordName, wordMean, sentenceDiff) => {
+const generateExample = async (apiKey, wordLang, wordName, wordMean) => {
   const configuration = new Configuration({
     apiKey: apiKey,
   });
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: `Create one simple short example sentence in the ${wordLang} language, always using the word ${wordName}, which means ${wordMean}. Include a Japanese translation at the end, and use only words of the ${sentenceDiff} difficulty level.` }],
+    messages: [{ role: "user", content: `Create one simple short example sentence in the ${wordLang} language, always using the word ${wordName}, which means ${wordMean}. Include a Japanese translation at the end, and use only words of the same difficulty level as ${wordName}.` }],
   });
   const response = { "content": completion.data.choices[0].message.content };
   return response;
